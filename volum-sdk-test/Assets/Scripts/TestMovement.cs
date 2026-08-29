e9712af4ba8af2e7ee7bac197445cae1c925e296
Marc <magueracoras@gmail.com>
2026-08-29 11:10:04 +0200

    test movement with jobs parallel system

--- volum-sdk-test/Assets/Scripts/TestMovement.cs
+++ volum-sdk-test/Assets/Scripts/TestMovement.cs
@@ -1,30 +1,26 @@
+using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
-public class TestMovement : MonoBehaviour
+public struct TransformJob : IJobParallelFor
 {
-    //private float _radius = 5f;
-    private float _time;
-    private float _speed = 10;
-    private float _noiseFrequency = 0.1f;
-    private float3 basePos;
+    public float Time;
+    public float Speed;
+    public float NoiseFrequency;
+    public NativeArray<float3> BasePos;
+    public NativeArray<float3> OutPut;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //  Execute in parallel
+    public void Execute(int index)
     {
-        basePos = transform.position;
-    }
+        float3 basePos = BasePos[index];
+        float acceleration = Time * Speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        _time += Time.deltaTime;
-        float acceleration = _time * _speed;
-
-        float x = noise.snoise(new float4(basePos * _noiseFrequency, acceleration));
-        float y = noise.snoise(new float4(basePos.yzx * _noiseFrequency, acceleration + 30f));
-        float z = noise.snoise(new float4(basePos.zxy * _noiseFrequency, acceleration + 70f));
+        float x = noise.snoise(new float4(basePos * NoiseFrequency, acceleration));
+        float y = noise.snoise(new float4(basePos.yzx * NoiseFrequency, acceleration + 30f));
+        float z = noise.snoise(new float4(basePos.zxy * NoiseFrequency, acceleration + 70f));
 
         float3 newPos = basePos + new float3(x, y, z) * 0.6f;
 
@@ -33,6 +29,70 @@ public class TestMovement : MonoBehaviour
         float sn = math.sin(angle);
         newPos = new float3(newPos.x * cs - newPos.z * sn, newPos.y, newPos.x * sn + newPos.z * cs);
 
-        transform.position = newPos;
+        OutPut[index] = newPos;
+    }
+}
+
+public class TestMovement : MonoBehaviour
+{
+    private JobHandle _handle;
+    public Transform[] spheres;
+
+    public float _speed = 10;
+    public float _noiseFrequency = 1;
+
+    public NativeArray<float3> basePos;
+    public NativeArray<float3> result;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // Persistent: vive hasta que lo liberemos manualmente (Temp solo dura el frame/scope actual)
+        basePos = new NativeArray<float3>(spheres.Length, Allocator.Persistent);
+        result = new NativeArray<float3>(spheres.Length, Allocator.Persistent);
+
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            basePos[i] = spheres[i].transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Completamos el job del frame anterior antes de tocar los arrays o programar uno nuevo
+        _handle.Complete();
+
+        // Aplicamos el resultado del job ya completado a las esferas
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            spheres[i].position = result[i];
+        }
+
+        TransformJob job = new TransformJob
+        {
+            Time = Time.time,
+            Speed = _speed,
+            NoiseFrequency = _noiseFrequency,
+            BasePos = basePos,
+            OutPut = result
+        };
+
+        _handle = job.Schedule(spheres.Length, 64);
+    }
+
+    void LateUpdate()
+    {
+        // Nos aseguramos de que el job termine antes de que el frame acabe
+        _handle.Complete();
+    }
+
+    void OnDestroy()
+    {
+        _handle.Complete();
+
+        if (basePos.IsCreated) basePos.Dispose();
+        if (result.IsCreated) result.Dispose();
     }
 }
--- volum-sdk-test/Packages/com.marc-aguera-garcia.volumsdk/Runtime/Public/VolumFrame.cs
+++ volum-sdk-test/Packages/com.marc-aguera-garcia.volumsdk/Runtime/Public/VolumFrame.cs
@@ -9,53 +9,6 @@ namespace Volum.SDK
     /// </summary>
     public class VolumFrame
     {
-        public VolumPoint[] volumPoints = new VolumPoint[100];
-        private float3[] basePos = new float3[100];
-        private float _radius = 5f;
-        private float _time;
-        private float _speed;
-        private float _noiseFrequency;
 
-
-        public void Initialize()
-        {
-            for (int i = 0; i < basePos.Length; i++)
-            {
-                VolumPoint point = new VolumPoint();
-
-                Random rnd = Random.CreateFromIndex(1);
-                
-                float3 dir = rnd.NextFloat3Direction();
-                float r = math.pow(rnd.NextFloat(), 1f / 3f) * _radius;
-
-                basePos[i] = dir * r;
-            }
-        }
-
-        public void UpdateFrame()
-        {
-            for (int i = 0; i < volumPoints.Length; i++)
-            {
-
-                volumPoints[i] = TranslatePoint(basePos[i]);
-            }
-        }
-
-        // test movement
-        public VolumPoint TranslatePoint(float3 basePos)
-        {
-            float acceleration = _time * _speed;
-
-            float x = noise.snoise(new float4(basePos * _noiseFrequency, acceleration));
-            float y = noise.snoise(new float4(basePos.yzx * _noiseFrequency, acceleration + 31.7f));
-            float z = noise.snoise(new float4(basePos.zxy * _noiseFrequency, acceleration + 71.3f));
-
-            float3 newPos = basePos + new float3(x, y, z) * 0.6f;
-
-            // test color red
-            float4 color = new float4(255f, 0f, 0f, 255f);
-
-            return new VolumPoint { Position = newPos, Color =  color};
-        }
     }
 }
